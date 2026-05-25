17b8272cbd17cf49d7b304c04d96409b7f566ef8
Daniel Kliewer <danielkliewer@gmail.com>
2026-05-24 20:55:52 -0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,14 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [],
   },
+  async rewrites() {
+    return [
+      {
+        source: "/favicon.ico",
+        destination: "/icon",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
